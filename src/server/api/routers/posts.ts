6041eec31cbba0b77be4f67b8e549afac9a14e60
owleyeview <rashaanlightpool@gmail.com>
2023-07-20 20:26:53 -0700

    UX improvements

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -63,7 +63,7 @@ export const postsRouter = createTRPCRouter({
   create: privateProcedure
   .input(
     z.object({
-        content: z.string().emoji().min(1).max(280),
+        content: z.string().emoji("Emojis only baby").min(1).max(280),
     })
     )
     .mutation(async ({ ctx, input }) => {
