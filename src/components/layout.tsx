23e5654f7d135cb9ad4b7ad19c163bb2b577351a
owleyeview <rashaanlightpool@gmail.com>
2023-07-24 12:10:06 -0700

    fixed vertical borders, now present on scroll

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,8 +3,8 @@ import type { PropsWithChildren } from "react"
 
 export const PageLayout = (props: PropsWithChildren) => {
     return (
-        <main className="flex justify-center h-screen">
-            <div className="h-full w-full md:max-w-2xl border-x border-slate-400 overflow-y-scroll">  
+        <main className="overflow-none flex justify-center min-h-screen">
+            <div className="h-full w-full md:max-w-2xl border-x border-slate-400">  
                 {props.children}
             </div>
         </main>
